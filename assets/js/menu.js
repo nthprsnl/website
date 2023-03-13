961b6c0e55c0a0e0f73ae05cc3cb92bb533d38a3
nothing personal <nthprsnl@gmail.com>
2023-03-14 01:57:43 +0800

    I NEED TO SLEEP WRAAGH

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,4 +1,4 @@
-function changeMenu(menu) {
+function changeMenu(event, menu) {
     menucontent = document.getElementsByClassName("menuContent");
     for (i = 0; i < menucontent.length; i++) {
         menucontent[i].style.display = "none";
@@ -6,4 +6,25 @@ function changeMenu(menu) {
 
     currentElement = document.getElementsByClassName(menu)[0]
     currentElement.style.display = "block";
+
+    document.getElementById('title').innerHTML = event.target.innerHTML
+}
+
+let toggleMenu = false;
+function toggle() {
+    if (toggleMenu == false) {
+            document.getElementsByClassName("currentMenu")[0].style.display = "none";
+            document.getElementsByClassName("menus")[0].style.display = "flex";
+    } else if (toggleMenu == true) {
+            document.getElementsByClassName("currentMenu")[0].style.display = "flex";
+            document.getElementsByClassName("menus")[0].style.display = "none";
+    }
+    toggleMenu = !toggleMenu;
+}
+
+toggle()
+changeMenu('featured')
+
+function sex() {
+    return console.log('thanks for checking the source code lmao')
 }
